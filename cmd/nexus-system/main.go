54be43266c0e1a30c4c82f46639e53ef928d095f
amuick <amuick@gmail.com>
2022-01-12 19:51:51 -0600

    setup basic healthcheck

---
+++ cmd/nexus-system/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+
+	"log"
+	"net/http"
+)
+
+var logger log.Logger
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ping", ping).Methods("GET")
+
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		logger.Fatalf("error %v", err)
+	}
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nexus-system
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
